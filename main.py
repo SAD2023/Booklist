0e9b46ae0f41358017eb816701a7d1d356ecdc34
Sadman <skc86@cornell.edu>
2022-01-17 22:09:25 -0500

    added a generate list button that creates a pdf of all the information from firebase

--- main.py
+++ main.py
@@ -7,15 +7,32 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 from PyQt5.QtGui import QIntValidator,QDoubleValidator,QFont
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
-
+from fpdf import FPDF
 
 # Using a service account
-cred = credentials.Certificate('booklist-163c0-firebase-adminsdk-ndzn5-3320ed0021.json')
+cred = credentials.Certificate('')
 firebase_admin.initialize_app(cred)
 
 # Setting up the Database
 db = firestore.client()
 
+""" 
+  Creates a pdf with the provided text.
+
+  Parameters: 
+    text: string
+"""
+def make_pdf(text):
+  pdf = FPDF()
+
+  pdf.add_page()
+
+  pdf.set_font("Arial", size = 15)
+    
+  pdf.multi_cell(200, 10, txt = text, 
+          align = 'L')
+
+  pdf.output("books.pdf")   
 
 """ 
  Takes several QT Widgets and their text inputs and adds the corresponding info
@@ -39,22 +56,105 @@ def upload(field1, name, field2, rating, field3, comment):
     "thoughts": comment
 })
 
+"""
+Gets the list of books and their corresponding information.
+
+"""
+def download():
+  doc_ref = db.collection('books')
+  docs = doc_ref.stream()
+  information = ""
+  for doc in docs:
+    information += (f'{doc.id} => {doc.to_dict()}')
+  
+  make_pdf(information)
+
+
+
 """  
-  The main function, which creates the layout, creates all of the widgets, and
+  The main class, which creates the layout, creates all of the widgets, and
   adds them to the layout.
 
   Parameters: none
 """
+class MainWindow():
+  def __init__(self):
+    app = QApplication([])
+    window = QWidget()
+    window.setFixedWidth(400)
+    window.setFixedHeight(250)
+
+    label = QLabel("Add a book!")
+    label.setFont(QFont("Courier New", 23))
+
+    layout = QVBoxLayout()
+
+    button1 = QPushButton('Add')
+    button2 = QPushButton('Generate List')
+    
+    e1 = QLineEdit()
+    e1.setAlignment(Qt.AlignRight)
+    e1.setFont(QFont("Courier New",15))
+    e1.setPlaceholderText("Name of the book ") 
+
+    
+    e2 = QLineEdit()
+    e2.setAlignment(Qt.AlignRight)
+    e2.setFont(QFont("Courier New",15))
+    e2.setPlaceholderText("A Rating for the book ") 
+
+      
+    e3 = QLineEdit()
+    e3.setAlignment(Qt.AlignRight)
+    e3.setFont(QFont("Courier New",15))
+    e3.setPlaceholderText("Thoughts on the book ") 
+
+    layout.addWidget(label)
+    layout.addWidget(e1)
+    layout.addWidget(e2)
+    layout.addWidget(e3)
+    layout.addWidget(button1)
+    layout.addWidget(button2)
+
+    
+    button1.clicked.connect(lambda: upload(e1, e1.text(), e2, e2.text(), e3, e3.text()))
+    button2.clicked.connect(lambda : download())
+
+
+    window.setLayout(layout)
+    window.show()
+    app.exec()
+  
+  def show_new_window(self):
+    w = AnotherWindow()
+    w.show()
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+"""
 def window():
   app = QApplication([])
-  window = QWidget()
+  window = QStackedWidget()
+  window.addWidget(SecondWindow)
   window.setFixedWidth(400)
   window.setFixedHeight(250)
+
   label = QLabel("Add a book!")
   label.setFont(QFont("Courier New", 23))
   layout = QVBoxLayout()
+
   button1 = QPushButton('Add')
-  layout.addWidget(label)
+  button2 = QPushButton('Change')
 
   e1 = QLineEdit()
   e1.setAlignment(Qt.AlignRight)
@@ -73,18 +173,23 @@ def window():
   e3.setFont(QFont("Courier New",15))
   e3.setPlaceholderText("Thoughts on the book ") 
 
+  layout.addWidget(label)
   layout.addWidget(e1)
   layout.addWidget(e2)
   layout.addWidget(e3)
   layout.addWidget(button1)
+  layout.addWidget(button2)
 
   
   button1.clicked.connect(lambda: upload(e1, e1.text(), e2, e2.text(), e3, e3.text()))
+  button2.clicked.connect(lambda : window.setCurrentIndex(window.currentIndex()+1))
 
 
   window.setLayout(layout)
   window.show()
   app.exec()
+"""
 
 # Starts running the program
-window()
+if __name__=='__main__':
+  MainWindow()
